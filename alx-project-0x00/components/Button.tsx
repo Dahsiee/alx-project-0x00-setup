8048e4a1b3b062ab2eca93e918cd17535c0a0ddb
Dahsiee <chuksgabriella2019@gmail.com>
2025-11-14 18:10:13 +0000

    fix: update Button component styles for improved appearance

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -3,7 +3,7 @@ import { ButtonProps } from "@/interfaces";
 
 const Button: React.FC<ButtonProps> = ({title,styles}) => {{
   return(
-    <button className= "border px-3 py-1 text-lg mt-3 bg-blue-500 text-white rounded-full">{title}</button>
+    <button className= "border px-3 py-1 text-lg mt-3 bg-blue-500 text-white rounded-full rounded-sm rounded-lg">{title}</button>
   )
 }
 }
